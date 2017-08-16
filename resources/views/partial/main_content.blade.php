64d126088ff780191b104d100c5be9b9f652412f
duongtran1706 <duongtran1706@gmail.com>
2017-08-17 02:36:11 +0700

    commit 17/8/2017

--- resources/views/partial/main_content.blade.php
+++ resources/views/partial/main_content.blade.php
@@ -11,14 +11,37 @@
     <ol class="carousel-indicators">
         <?php $i=0?>
         @while($i<count($post))
-            @if ($i==0)
-            <li data-target="#touch-silder" data-slide-to="{{$i}}"  class="active" ></li>
-            @else
-                <li data-target="#touch-silder" data-slide-to="{{$i}}" ></li>
-            @endif
-            <?php $i++ ?>
+            <li data-target="#touch-slider" data-slide-to="{{$i}}" <?php if($i==0) echo "class='active'"; $i++?>></li>
+
         @endwhile
-
-
     </ol>
+    <!--wrapper for slider-->
+    <div class="carousel-inner" role="listbox">
+    <?php $j=0 ?>
+    @foreach($post as $posts)
+    <div class="item <?php if($j==0) echo "active"; $j++ ?>">
+        <img src="{{ $posts->Picture }}" class="slide-image" alt=""/>
+        <div class="bs-slider-overlay"></div>
+        <div class="row">
+            <!--slide text layer-->
+            <div class="slide-text slide_style_left">
+                <h1 data-animation="animated zoomInRight"> {{$posts->Title}}</h1>
+                <p data-animation="animated fadeInLeft">{{$posts->Description}}</p>
+
+            </div>
+        </div>
+    </div>
+
+        @endforeach
+    </div>
+    <!-- left control-->
+    <a class=" left carousel-control" href="#touch-slider" role="button" data-slide="prev">
+        <span class="fa fa-angle-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <!-- right control-->
+    <a class="right carousel-control" href="#touch-slider" role="button" data-slide="next">
+        <span class="fa fa-angle-right" aria-hidden="true"></span>
+        <span class="sr-only">next</span>
+    </a>
 </div>
